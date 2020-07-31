a899151f3f90c8770273886c3079e7fe676bcc59
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-31 02:48:44 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ class GetRequester
     @url = url
   end
   
-  def get_response_body(url)
+  def get_response_body
     uri = URI.Parse(url)
     response = Net::HTTP.get_response(uri)
     response.body
